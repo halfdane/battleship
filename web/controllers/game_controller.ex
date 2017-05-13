d5b1a3fdb4c34e1f506f0c9a2b22df257b890a70
halfdane <halfdane@gmx.net>
2017-05-13 16:38:20 +0200

    generate new game-state for new games and store the user's name in there

--- web/controllers/game_controller.ex
+++ web/controllers/game_controller.ex
@@ -2,15 +2,21 @@ defmodule Battleship.GameController do
   use Battleship.Web, :controller
 
   def random_string(length) do
-    :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
+    :crypto.strong_rand_bytes(length) |> Base.url_encode64
   end
 
   def show(conn, %{"id" => game_id}) do
-    render conn, "game.html", game_id: game_id
+    username = Agent.get(String.to_atom(game_id), &Map.get(&1, :username))
+    render conn, "game.html", game_id: game_id, username: username
   end
 
-  def create(conn, %{"username" => messenger}) do
-    render conn, "game.html", messenger: messenger, game_id: random_string(10)
+  def create(conn, %{"username" => username}) do
+    game_id = String.to_atom(random_string(16))
+
+    {:ok, _pid} = Agent.start_link(fn -> %{} end, name: game_id)
+    Agent.update(game_id, &Map.put(&1, :username, username))
+
+    redirect conn, to: Battleship.Router.Helpers.game_path(conn, :show, game_id)
   end
 
 end
