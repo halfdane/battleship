c5980806ae82dac81b4d033489e0ec570eaca08b
halfdane <halfdane@gmx.net>
2017-05-13 15:03:54 +0200

    input field for username and a button

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -1,6 +1,3 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "web/static/js/app.js".
-
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
 import {Socket} from "phoenix"
