9c971787dd2de7c0440f585957db442ee95182a8
halfdane <halfdane@gmx.net>
2017-05-14 10:50:41 +0200

    clean up tests

--- test/controllers/game_controller_test.exs
+++ test/controllers/game_controller_test.exs
@@ -1,21 +1,21 @@
 defmodule Battleship.GameControllerTest do
   use Battleship.ConnCase
 
-  test "can create new game", %{conn: conn} do
+  test "should create new game", %{conn: conn} do
     conn = post conn, game_path(conn, :create), [ username: "some_username" ]
     assert redirected_to(conn) =~ ~r|/games/[^/]*|
   end
 
-  test "Create a new game and then connect", %{conn: conn} do
+  test "should redirect to valid show-function", %{conn: conn} do
+    conn = post conn, game_path(conn, :create), [ username: "some_username" ]
+    "/games/" <> id = redirected_to(conn)
+    assert redirected_to(conn) == game_path(conn, :show, URI.decode(id))
+  end
+
+  test "New game should know user's name", %{conn: conn} do
     expected_username = "some_327192_username"
     conn = post conn, game_path(conn, :create), [ username: expected_username ]
-
-    [location] = Plug.Conn.get_resp_header(conn, "location")
-    "/games/" <> id = location
-
-    assert location == game_path(conn, :show, URI.decode(id))
-
-    conn = get conn, location
+    conn = get conn, redirected_to(conn)
     assert html_response(conn, 200) =~ expected_username
   end
 end
