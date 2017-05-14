3865f51b074928501290a0efb2cd4077f7256102
halfdane <halfdane@gmx.net>
2017-05-14 09:47:21 +0200

    have some meaningful tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,7 +1,12 @@
 defmodule Battleship.PageControllerTest do
   use Battleship.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "Can connect to main", %{conn: conn} do
+    response = get conn, "/"
+    assert response.status == 200
+  end
+
+  test "Main title exists", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Welcome to Battleship!"
   end
--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -1,3 +1,22 @@
 defmodule Battleship.PageViewTest do
   use Battleship.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders an input field for username" do
+    html = render_to_string(Battleship.PageView, "index.html", [])
+    assert html =~ ~r|<input name="username".*>|
+   end
+
+  test "renders a submit button" do
+    html = render_to_string(Battleship.PageView, "index.html", [])
+    assert html =~ ~r|<button id="new_game_starter">[^<]*</button>|
+  end
+
+  test "renders the form" do
+    html = render_to_string(Battleship.PageView, "index.html", [])
+    assert html =~ ~r|<form[^>]*action="/games"[^>]*method="post"[^>]*>|
+  end
+
 end
